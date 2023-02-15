2c483f4005cfd7fa360709000f170f507366e293
hnspakistan2 <hnspakistan2@gmail.com>
2023-02-15 12:12:09 +0500

    hello 6

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
     const users = JSON.parse(localStorage.getItem('users')) || {}
     const dispatch = useDispatch();
     const navigate = useNavigate()
-    console.log('Hello 2')
+    console.log('Hello 6')
 
 
 
